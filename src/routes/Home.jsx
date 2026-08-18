170d79c1c1c1dd3b969aa3b3f156cfd466c40301
Vitor <vitor_fernandes_pie@yahoo.com.br>
2026-08-18 17:11:03 -0300

    fix: adjust spacing

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,9 +4,9 @@ import Hero from '../components/Hero';
 
 export default function Home() {
     return (
-        <div>
+        <div className="flex min-h-screen flex-col">
             <Header />
-            <section className="flex w-full h-[calc(100vh-83px)] items-center flex-col md:flex-row">
+            <section className="flex w-full flex-1 items-center flex-col md:flex-row">
                 <div className="w-full md:w-2/3"><Hero /></div>
                 <div className="hidden md:block md:w-1/3"></div>
             </section>
