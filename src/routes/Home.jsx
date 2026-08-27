457ba9a53319114c8b9c51e6f474c323f51c8eea
Vítor Barra <vifebarra@outlook.com>
2026-08-27 10:57:58 -0300

    feat: add footer

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,26 +1,29 @@
-import { Link } from 'react-router-dom';
-import Header  from '../components/Header'
-import Hero from '../components/Hero';
-import RecentPosts from '../components/RecentPosts';
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import RecentPosts from "../components/RecentPosts";
 import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
 
 export default function Home() {
-    return (
-        <div className="flex flex-col">
-            <Header />
-            <section className="flex w-full md:min-h-screen flex-1 items-center flex-col md:flex-row">
-                <div className="w-full md:w-2/3"><Hero /></div>
-                <div className="hidden md:block md:w-1/3"></div>
-            </section>
-
-            <section className='flex w-full flex-col px-8 py-16 md:flex-row md:gap-12'>
-                <div className='w-full md:w-2/3'>
-                    <RecentPosts />
-                </div>
-                <div className='w-full md:w-1/3'>
-                    <Sidebar />
-                </div>
-            </section>
+  return (
+    <div className="flex flex-col">
+      <Header />
+      <section className="flex w-full md:min-h-screen flex-1 items-center flex-col md:flex-row">
+        <div className="w-full md:w-2/3">
+          <Hero />
         </div>
-    );
+        <div className="hidden md:block md:w-1/3"></div>
+      </section>
+
+      <section className="flex w-full flex-col px-8 py-16 md:flex-row md:gap-12">
+        <div className="w-full md:w-2/3">
+          <RecentPosts />
+        </div>
+        <div className="w-full md:w-1/3">
+          <Sidebar />
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
 }
